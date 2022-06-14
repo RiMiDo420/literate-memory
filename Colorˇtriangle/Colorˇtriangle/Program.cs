0eb2a7177e4ed996f8b2f565e681823dbc4c50ec
Richard Dobíšek <59091715+RiMiDo420@users.noreply.github.com>
2022-06-14 21:51:46 +0200

    Add files via upload

---
+++ Colorˇtriangle/Colorˇtriangle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Colorˇtriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(EvaluateTriangle(Console.ReadLine()));
+            }
+        }
+
+        public static char EvaluateTriangle(string currentRow)
+        {
+            char[] rowAr = currentRow.ToCharArray();
+            string returnString = "";
+            if(rowAr.Length == 1)
+            {
+                return rowAr[0];
+            }
+            if (rowAr.Length == 0)
+            {
+                return 'X';
+            }
+            for (int i = 0; i<(rowAr.Length-1); i++)
+            {
+                string currentPair = rowAr[i].ToString() + rowAr[i + 1].ToString();
+                if(currentPair == "RR" || currentPair == "GB" || currentPair == "BG")
+                {
+                    returnString += "R";
+                }
+                else if (currentPair == "GG" || currentPair == "RB" || currentPair == "BR")
+                {
+                    returnString += "G";
+                }
+                else if (currentPair == "BB" || currentPair == "GR" || currentPair == "RG")
+                {
+                    returnString += "B";
+                }
+            }
+            return EvaluateTriangle(returnString);
+        }
+    }
+}
